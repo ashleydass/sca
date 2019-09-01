2a7dc15807e8cb6315088cf64115f468004fc2e0
Ashley Dass <ashley_dass@yahoo.com>
2019-09-01 23:00:45 +1000

    Added base React app with simple test

---
+++ client/src/App.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+import { findByTestAttribute } from './Util';
+
+describe('App Component', () => {
+  it('Renders without errors', () => {
+    const component = shallow(<App />);
+    expect(component.length).toBe(1);
+  })
+
+  it('Contains AppComponent', () => {
+    const component = shallow(<App />);
+    const appComponent = findByTestAttribute(component, 'AppComponent');
+    expect(appComponent.length).toBe(1);
+  })
+})
---
+++ client/src/Util/index.js
@@ -0,0 +1,1 @@
+export const findByTestAttribute = (component, attr) => component.find(`[data-test='${attr}']`);
